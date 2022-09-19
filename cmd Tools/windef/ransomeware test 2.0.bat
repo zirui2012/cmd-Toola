1fdf5eff6a8b1ecf82af7b16604ad287f9648e06
zirui2012 <109861667+zirui2012@users.noreply.github.com>
2022-09-19 20:48:57 +0800

    Add files via upload

---
+++ cmd Tools/windef/ransomeware test 2.0.bat
@@ -0,0 +1,27 @@
+@echo off
+echo hi this script will test windows defenders ransomeware but it will not use your files
+pause
+cls
+echo make sure windows defender ransomeware protection protectets your desktop and run this file directly from your desktop
+echo run it as adminatistator to make it more secure
+pause
+cls
+echo start?
+pause
+cls
+color a
+echo starting 1 creating folder
+echo scanning 20 trying to find folder
+echo scanning 45 scaning folders windows defender protects
+echo scanning 50 trying to create folders in protected folders
+echo removing 70 removing folders if have in the others folders
+echo finshing 95 scanning
+echo completed 100%
+pause
+color 7
+cls
+mkdir test
+echo if you can see the error msg on top or the notification from windows defender you are safe
+echo or get help online
+echo this is made by zirui
+pause
